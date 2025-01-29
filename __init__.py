54f0e940fbc9c0e3e67c20cf7a133d1f1b88e727
stration <34169333+stration@users.noreply.github.com>
2025-01-29 04:31:31 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
-from .plugin_config import config
+from netbox.plugins import PluginConfig
+from .version import __version__
 
 class CableTracertConfig(PluginConfig):
     name = 'cable_tracert'
     verbose_name = 'Cable Tracer'
     description = 'A plugin to trace cables to their endpoints in NetBox.'
     version = '0.1'
+    author = 'Igor Mentoyan'
+    author_email = 'stration75@gmail.com'
     base_url = 'cable-tracert'
+    min_version = '4.1.0'
+    max_version = '4.2.99'
 
 config = CableTracertConfig
