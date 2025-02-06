c978b058c4f5b13692f5085f1e9fdb2f7271b9d2
stration <34169333+stration@users.noreply.github.com>
2025-02-06 15:34:14 +0300

    plugin_config.py

--- plugin_config.py
+++ plugin_config.py
@@ -1,10 +1,11 @@
+# cable_tracer/plugin_config.py
 from netbox.plugins import PluginConfig
 
 class CableTracertConfig(PluginConfig):
-    name = 'cable_tracert'
+    name = 'cable_tracer'
     verbose_name = 'Cable Tracer'
     description = 'A plugin to trace cables to their endpoints in NetBox.'
     version = '0.1'
-    base_url = 'cable-tracert'
+    base_url = 'cable-tracert'  # Префикс для всех маршрутов плагина
 
 config = CableTracertConfig
